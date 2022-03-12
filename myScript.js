0e26838d612f89b07f017c645187ca9c3c10b959
machawt <mark@holt5.com>
2022-03-13 05:11:10 +0930

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,23 +1,8 @@
 var syl="no";
 
-function cma(syl){window.alert(syl);var etext=document.getElementById("echo");
+function cma(syl) {var etext=document.getElementById("echo");
 etext.innerHTML = etext.innerHTML+syl;}
-function cnga(){var etext=document.getElementById("echo");
-etext.innerHTML = etext.innerHTML+"nga";}
-function cti(){var etext=document.getElementById("echo");
-etext.innerHTML = etext.innerHTML+"ti";}ti
-function cka(){var etext=document.getElementById("echo");
-etext.innerHTML = etext.innerHTML+"ka";}
-function cs(){var etext=document.getElementById("echo");
-etext.innerHTML = etext.innerHTML+".";}
-function cpi(){var etext=document.getElementById("echo");
-etext.innerHTML = etext.innerHTML+"pi";}
-function cPi(){var etext=document.getElementById("echo");
-etext.innerHTML = etext.innerHTML+"Pi";}
-function cli(){var etext=document.getElementById("echo");
-etext.innerHTML = etext.innerHTML+"li";}
-function csp(){var etext=document.getElementById("echo");
-etext.innerHTML = etext.innerHTML+" ";}
+
 function next() {var test=document.getElementById("echo");re= document.getElementById("read");
 if(test.innerText===re.innerText){window.location.href = "p2.html";}}
 
