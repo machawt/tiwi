e131a38c021ead5b5ed4c030d23139f175227b9f
machawt <mark@holt5.com>
2022-03-13 20:27:14 +0930

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -6,5 +6,6 @@ etext.innerHTML = etext.innerHTML+syl;}
 function next() {var test=document.getElementById("echo");re= document.getElementById("read");
 if(test.innerText===re.innerText){window.location.href = "p2.html";}}
 
-
+function next3() {var test=document.getElementById("echo");re= document.getElementById("read");
+if(test.innerText===re.innerText){window.location.href = "p3.html";}}
 
