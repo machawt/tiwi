0997996bfc7d221db1f7b3febf9ba571a92ac288
machawt <mark@holt5.com>
2022-03-20 17:32:50 +0930

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -2,10 +2,13 @@ var syl="no";cnt=-1
 let txt=["Pika kalikali kapi makatinga.","Kamunga kalikali jiyima kapi makatinga? Marri Pika.","Pika maka kalikali jiyima? Kapi makatinga.",
          "Pulangumwaka papi jiyimi kapi makatinga.","Ninkiyi, pika kalikali jiyimi kapi yawurlama.",
          "Pulangumwaka kalikali jiyimi kapi yawurlama.","Ninkiyi, Palikila papi yimi kapi makatinga."
-,"Makanjiyimi pulangawaka? yimi Palikila.",        ]
+,"Maka jiyimi pulangawaka? yimi Palikila.","Ninkiyi, Mikirrami papi yimi kapi makatinga.",
+         "Maka jiyimi pulangawaka? yimi Mikirrami.","Arrimi kapi yawurlama, yimi Palikila.",
+         "Ninkiyi, Mikirrami kali yimi kapi yawurlama.",
+        ]
 let i1=["pika.jpg","pika.jpg",,"makatinga.jpg","pulangumwaka.jpg","pulangumwaka.jpg","yawurlama.jpg","yawurlama.jpg"
-       "pali.jpg","pali.jpg"]
-let i2=["makatinga.jpg","","","","","","",""]
+       "pali.jpg","pali.jpg","miki.jpg","miki.jpg","pali.jpg","yawrulama.jpg"]
+let i2=["makatinga.jpg","","","","","","","","","","",""]
 function cma(syl) {var etext=document.getElementById("echo");
 etext.innerHTML = etext.innerHTML+syl;}
 function clr()  {var etext=document.getElementById("echo");
