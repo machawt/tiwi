18bc5cfed0c145d20f2eee4b2e3669c1975b6ba7
machawt <mark@holt5.com>
2022-03-20 15:23:56 +0930

    Add files via upload

--- myScript.js
+++ myScript.js
@@ -1,14 +1,24 @@
-var syl="no";
-
+var syl="no";cnt=-1
+let txt=["Pika kalikali kapi makatinga.","Pulangumwaka papi jiyimi kapi makatinga.","Ninkiyi, pika kalikali jiyimi kapi yawurlama."]
+let i1=["pika.jpg","pulangumwaka.jpg","pulangumwaka.jpg","yawurlama.jpg"]
+let i2=["makatinga.jpg","",""]
 function cma(syl) {var etext=document.getElementById("echo");
 etext.innerHTML = etext.innerHTML+syl;}
-
-function next() {var test=document.getElementById("echo");re= document.getElementById("read");
-if(test.innerText===re.innerText){window.location.href = "p2.html";}}
-
-function next3() {var test=document.getElementById("echo");re= document.getElementById("read");
-if(test.innerText===re.innerText){window.location.href = "p3.html";}}
+function clr()  {var etext=document.getElementById("echo");
+etext.innerHTML = "";}
+//function hd() {var re= document.getElementById("read");if(re.hidden===True){re.hidden=False;};
+//else{re.hidden=True;}}
 
 
-function ReplacingImage(){ document.getElementById("i1").src="image2.png" 
-} 
+function next1() {var test=document.getElementById("echo");re= document.getElementById("read");if(test.innerText===re.innerText){cnt=cnt+1;
+re.innerText=txt[cnt];
+document.getElementById("im1").src=i1[cnt];
+document.getElementById("im2").src=i2[cnt]}}
+
+function back1() {var re= document.getElementById("read");cnt=cnt-1;
+re.innerText=txt[cnt];
+document.getElementById("im1").src=i1[cnt];document.getElementById("im2").src=i2[cnt]}
+
+
+//function ReplacingImage(){ document.getElementById("i1").src="image2.png" 
+//} 
