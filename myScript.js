b28325e1af6c9233876a51edc60617b44347dcb7
machawt <mark@holt5.com>
2022-03-20 13:07:10 +0930

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -9,3 +9,6 @@ if(test.innerText===re.innerText){window.location.href = "p2.html";}}
 function next3() {var test=document.getElementById("echo");re= document.getElementById("read");
 if(test.innerText===re.innerText){window.location.href = "p3.html";}}
 
+
+function ReplacingImage(){ document.getElementById("i1").src="image2.png" 
+} 
