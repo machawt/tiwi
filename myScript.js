efd3c12c0b5c58bf73a178cd20b9212768db4b04
machawt <mark@holt5.com>
2022-04-10 14:57:59 +0800

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -5,10 +5,11 @@ let txt=["Pika kalikali kapi makatinga.","Pulangumwaka papi jiyimi kapi makating
 let i1=["pika.jpg","pulangumwaka.jpg","pulangumwaka.jpg","yawurlama.jpg"]
 let i2=["makatinga.jpg","",""]
 let k1=["ka","ma","nga"]
+let k2=["li","pi","Pi","ti"]
+let k3=[]
 
-
-
-function keys (){
+function keys{
+function keys1(){
 line1=document.getElementById("key1");
 for (let loop1= 0; loop1< k1.length; loop1++) 
 { 
@@ -20,6 +21,29 @@ btn.classList.add("b");
 line1.appendChild(btn);
 
 }}
+
+function keys2(){
+line1=document.getElementById("key2");
+for (let loop1= 0; loop1< k2.length; loop1++) 
+{ 
+let btn = document.createElement("button");
+btn.innerHTML = k1[loop1];
+btn.addEventListener("click", cma(k2[loop1]));
+btn.classList.add("b");
+line1.appendChild(btn);
+}}
+
+function keys3(){
+line1=document.getElementById("key3");
+for (let loop1= 0; loop1< k3.length; loop1++) 
+{ 
+let btn = document.createElement("button");
+btn.innerHTML = k1[loop1];
+btn.addEventListener("click", cma(k3[loop1]));
+btn.classList.add("b");
+line1.appendChild(btn);
+}}
+}
 function cma(syl) {let etext=document.getElementById('echo');etext.innerHTML = etext.innerHTML+syl;}
 function clr()  {let etext=document.getElementById('echo');let sld=etext.innerHTML;etext.innerHTML=sld.slice(0,-1);}
 function hd() {if (re.style.visibility==="visible"){re.style.visibility="hidden";}
