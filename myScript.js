7c83b4c42fa31c9f3dc46b854a5185729f969933
machawt <mark@holt5.com>
2022-04-10 19:16:14 +0800

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -6,20 +6,20 @@ let i1=["pika.jpg","pulangumwaka.jpg","pulangumwaka.jpg","yawurlama.jpg"]
 let i2=["makatinga.jpg","",""]
 let k1=["ka","ma","nga"]
 let k2=["li","pi","Pi","ti"]
-let k3=[""]
+let k3=[""];
 
 function keys(){
-function keys1(){
+
 line1=document.getElementById("key1");
 for (let loop1= 0; loop1< k1.length; loop1++) 
 { 
 let btn = document.createElement("button");
 btn.innerHTML = k1[loop1];
 //btn.addEventListener("click", cma(k1[loop1]));
-btn.classList.add("b");
+btn.classList.add("no b");
 line1.appendChild(btn);
 }}
-
+/*
 function keys2(){
 line1=document.getElementById("key2");
 for (let loop1= 0; loop1< k2.length; loop1++) 
@@ -41,7 +41,7 @@ btn.addEventListener("click", cma(k3[loop1]));
 btn.classList.add("b");
 line1.appendChild(btn);
 }}
-}
+}*/
 function cma(syl) {let etext=Thanks document.getElementById('echo');etext.innerHTML = etext.innerHTML+syl;}
 function clr()  {let etext=document.getElementById('echo');let sld=etext.innerHTML;etext.innerHTML=sld.slice(0,-1);}
 function hd() {if (re.style.visibility==="visible"){re.style.visibility="hidden";}
