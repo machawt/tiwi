5c8f72586f308043983ce7df00e1ffb0817fefd5
machawt <mark@holt5.com>
2022-04-11 19:41:54 +0800

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -4,7 +4,7 @@ let  re= "document.getElementById('read')"
 let txt=["Pika kalikali kapi makatinga.","Pulangumwaka papi jiyimi kapi makatinga.","Ninkiyi, pika kalikali jiyimi kapi yawurlama."]
 let i1=["pika.jpg","pulangumwaka.jpg","pulangumwaka.jpg","yawurlama.jpg"]
 let i2=["makatinga.jpg","",""]
-let k1=["ka","ma","nga"]
+let k1=[" ","ka","ma","nga"]
 let k2=["li","pi","Pi","ti"]
 let k3=[""];
 
@@ -31,39 +31,21 @@ btn.onclick = function write() {let etext=document.getElementById('echo');etext.
 //btn.addEventListener("click", cma("hi"));
 btn.classList.add("b");
 line1.appendChild(btn);
-}}
+}
 
-//}
-/*
+btn);
+}
 
-function keys2(){
-line1=document.getElementById("key2");
-for (let loop1= 0; loop1< k2.length; loop1++) 
-{ 
-let btn = document.createElement("button");
-btn.innerHTML = k2[loop1];
-btn.addEventListener("click", cma(k2[loop1]));
-btn.classList.add("b");
-line1.appendChild(btn);
-}}
-
-function keys3(){
 line1=document.getElementById("key3");
 for (let loop1= 0; loop1< k3.length; loop1++) 
 { 
 let btn = document.createElement("button");
 btn.innerHTML = k3[loop1];
-btn.addEventListener("click", cma(k3[loop1]));
+btn.onclick = function write() {let etext=document.getElementById('echo');etext.innerHTML = etext.innerHTML+ k3[loop1];};
 btn.classList.add("b");
 line1.appendChild(btn);
-}}
-}*/
-function cma(syl) {let etext=document.getElementById('echo');etext.innerHTML = etext.innerHTML+syl;}
-function clr()  {let etext=document.getElementById('echo');let sld=etext.innerHTML;etext.innerHTML=sld.slice(0,-1);}
-function hd() {if (re.style.visibility==="visible"){re.style.visibility="hidden";}
-else{re.style.visibility="visible";}}
-
-
+}
+}
 function next1() {var test=document.getElementById("echo");re= document.getElementById("read");if(test.innerText===re.innerText){cnt=cnt+1;
 re.innerText=txt[cnt];
 document.getElementById("im1").src=i1[cnt];
