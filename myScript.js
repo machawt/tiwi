d258c3f924b9c82861f0e4f5fc522166e5ed742f
machawt <mark@holt5.com>
2022-04-11 19:49:32 +0800

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,16 +1,14 @@
 let syl="no";cnt=-1
 let etext="document.getElementById('echo')"
 let  re= "document.getElementById('read')"
-let txt=["Pika kalikali kapi makatinga.","Pulangumwaka papi jiyimi kapi makatinga.","Ninkiyi, pika kalikali jiyimi kapi yawurlama."]
+let txt=["Pika_kalikali_kapi_makatinga.","Pulangumwaka_papi_jiyimi_kapi_makatinga.","Ninkiyi,_pika_kalikali_jiyimi_kapi_yawurlama."]
 let i1=["pika.jpg","pulangumwaka.jpg","pulangumwaka.jpg","yawurlama.jpg"]
 let i2=["makatinga.jpg","",""]
-let k1=[" ","ka","ma","nga"]
+let k1=["ka","ma","nga"]
 let k2=["li","pi","Pi","ti"]
-let k3=[""];
+let k3=["_",".",","];
 
 function keys(){
-//function keys1(){
-
 line1=document.getElementById("key1");
 for (let loop1= 0; loop1< k1.length; loop1++) 
 { 
