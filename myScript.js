9b045bde10df299a3b578ec90cb453eab11f63a3
machawt <mark@holt5.com>
2022-04-12 08:29:50 +0800

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -49,6 +49,7 @@ function back1() {var re= document.getElementById("read");cnt=cnt-1;
 re.innerText=txt[cnt];
 document.getElementById("im1").src=i1[cnt];document.getElementById("im2").src=i2[cnt]
 }
+function hde (){}
 function clr(){
 let str=document.getElementById("echo");
 del= str.innerText.slice(0, -1);
