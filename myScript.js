f5573b138cdd63f6c20f37018d379d5268962612
machawt <mark@holt5.com>
2022-04-18 18:40:55 +0930

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,67 +1,75 @@
-let syl="no";cnt=-1
-let etext="document.getElementById('echo')"
-let  re= "document.getElementById('read')"
-let txt=["Pika_kalikali_kapi_makatinga.","Pulangumwaka_papi_jiyimi_kapi_makatinga.","Ninkiyi,_pika_kalikali_jiyimi_kapi_yawurlama."]
-let i1=["pika.jpg","pulangumwaka.jpg","pulangumwaka.jpg","yawurlama.jpg"]
-let i2=["makatinga.jpg","",""]
-let k1=[["ka","ma","nga"],["ka","la","ma","nga","pa","wa"],["ka","rla","ma","nga","pa","wa"]]
-let k2=[["li","pi","Pi","ti"], ["ji","mi","pi","ti","yi"],["Nin", "ji","ki","li","mi","pi","ti","yi"]]
-let k3=[["_",".",","],["ngum","Pu","_",".",","],["wu","_",".",","]]
+let syl="no";cnt=-1;
+let etext=document.getElementById('echo');
+let  re= document.getElementById('read');
+let test=document.getElementById("echo");
+let engdiv=document.getElementById("english");
+
+let txt=["Pi*ka*_*ka*li*ka*li*_*ka*pi*_*ma*ka*ti*nga*.","Pu*la*ngum*wa*ka*_*pa*pi*_*ji*yi*mi*_*ka*pi*_*ma*ka*ti*nga*.","Nin*ki*yi*,*_*pi*ka*_*ka*li*ka*li*_*ji*yi*mi*_*ka*pi*_*ya*wu*rla*ma*."];
+let engs=["horse runs to stream","dog  arrived she did at stream","then horse run she did to bush "]
 
 function keys(){
 line1=document.getElementById("key1");
 line1.innerHTML="";
-for (let loop1= 0; loop1< k1[cnt].length; loop1++) 
+myArray = txt[cnt].split("*");
+butset = new Set(myArray);
+let butArray = Array.from(butset);
+butArray.sort();
+for (let loop1= 0; loop1< butArray.length; loop1++) 
 { 
 let btn = document.createElement("button");
-btn.innerHTML = k1[cnt][loop1];
-btn.onclick = function write() {let etext=document.getElementById('echo');etext.innerHTML = etext.innerHTML+ k1[cnt][loop1];};
+btn.innerHTML = butArray[loop1];
+btn.onclick = function write() {let etext=document.getElementById('echo');
+etext.innerHTML = etext.innerHTML+ butArray[loop1];};
 btn.classList.add("b");
 line1.appendChild(btn);
-}
+}};
 
-line1=document.getElementById("key2");
-line1.innerHTML="";
-for (let loop1= 0; loop1< k2[cnt].length; loop1++) 
-{ 
-let btn = document.createElement("button");
-btn.innerHTML = k2[cnt][loop1];
-btn.onclick = function write() {let etext=document.getElementById('echo');etext.innerHTML = etext.innerHTML+ k2[cnt][loop1];};
-btn.classList.add("b");
-line1.appendChild(btn);
-}
-
-
-line1=document.getElementById("key3");
-line1.innerHTML="";
-for (let loop1= 0; loop1< k3[cnt].length; loop1++) 
-{ 
-let btn = document.createElement("button");
-btn.innerHTML = k3[cnt][loop1];
-btn.onclick = function write() {let etext=document.getElementById('echo');etext.innerHTML = etext.innerHTML+ k3[cnt][loop1];};
-btn.classList.add("b");
-line1.appendChild(btn);
-}
-}
-function next1() {var test=document.getElementById("echo");re= document.getElementById("read");if(test.innerText===re.innerText){cnt=cnt+1;
-re.innerText=txt[cnt];
+function next1(){ 
+{var test=document.getElementById("echo");
+re= document.getElementById("read");
+if(test.innerText===re.innerText){cnt=cnt+1;
+    if (cnt>txt.length){cnt=cnt-1};
+clean=txt[cnt].replaceAll('*','');
+re.innerText=clean;
 test.innerText="";
-document.getElementById("im1").src=i1[cnt];
-document.getElementById("im2").src=i2[cnt]}
+engdiv=document.getElementById("english");
+engdiv.innerText=engs[cnt];}
 keys();
-}
-function back1() {var re= document.getElementById("read");cnt=cnt-1;
-if (cnt<0){cnt=0;}
-keys();
-re.innerText=txt[cnt];
-document.getElementById("im1").src=i1[cnt];document.getElementById("im2").src=i2[cnt]
-}
-//function hde (){re= document.getElementById("read");if(re.style.visibility==='hidden'){re.style.visibility='visible';}
-//else{re.style.visibility="hidden";}
+}}
+function fd(){ 
+    test=document.getElementById("echo");
+    re= document.getElementById("read");
+    cnt=cnt+1;if (cnt>txt.length){cnt=cnt-1};
+    clean=txt[cnt].replaceAll('*','');
+    re.innerText=clean;
+    test.innerText="";
+    engdiv=document.getElementById("english")
+    engdiv.innerText=engs[cnt];
+
+    keys();
+    }
+    
+function bk1(){ 
+    test=document.getElementById("echo");
+    re= document.getElementById("read");
+    cnt=cnt-1;if (cnt<0){cnt=0};
+    clean=txt[cnt].replaceAll('*','');
+    re.innerText=clean;
+    test.innerText="";
+    engdiv=document.getElementById("english")
+    engdiv.innerText=engs[cnt];
+    keys();
+    }
+
+function hd() {if (re.style.visibility==="visible"){re.style.visibility="hidden";}
+else{re.style.visibility="visible";}}
+
+function engvis() {if (engdiv.style.visibility==="visible"){engdiv.style.visibility="hidden";}
+else{engdiv.style.visibility="visible";}}
+
+
 function clr(){
 let str=document.getElementById("echo");
 del= str.innerText.slice(0, -1);
 str.innerText=del;
 }
-//function ReplacingImage(){ document.getElementById("i1").src="image2.png" 
-//} 
