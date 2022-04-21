c526eb37d8dc019c0d51198be963957d644f620b
machawt <mark@holt5.com>
2022-04-21 09:45:07 +0930

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,7 +1,8 @@
 let cnt=-1;
-let txt=["Pi*ka*_*ka*li*ka*li*_*ka*pi*_*ma*ka*ti*nga*.","Ti*rri*ka*_*ka*li*ka*li*_*ji*yi*mi*_*ka*pi*_*ta*ka*.",
+let txt=["Pi*ka*_*ka*li*ka*li*_*ka*pi*_*ma*ka*ti*nga*.","Ti*rri*ka*_*ka*li*ka*li*_*ji*yi*mi*_*ka*pi*_*ta*ka*.","mu*wa*ni*_*ka*li*ka*li*_*yi*mi*_*ka*pi*_*ti*ngi*wi*ni*.",
+         "Tin*nga*_*ka*li*ka*li*_*ji*yi*mi*_*ka*pi*_nga*rra*-*rri*nga*ni*.",
          "Pu*la*ngum*wa*ka*_*pa*pi*_*ji*yi*mi*_*ka*pi*_*ma*ka*ti*nga*.","Nin*ki*yi*,*_*pi*ka*_*ka*li*ka*li*_*ji*yi*mi*_*ka*pi*_*ya*wu*rla*ma*."];
-let engs=["horse runs to stream","snake ran she did on branch?",
+let engs=["horse runs to stream","green tree snake ran she did to tree/plant.","goanna (kawarri) he ran to the (black) man.","the woman ran she did to his? father.",
           "dog  arrived she did at stream","then horse run she did to bush "];
 
 function next1(){
